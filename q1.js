7edac7297e7ef64773d1de4ddce10b1fd1ef4136
naziaali1525 <naziaali688@gmail.com>
2018-06-20 17:07:03 -0700

    solution

---
+++ q1.js
@@ -0,0 +1,6 @@
+function calculateSquare() {
+
+    let inputValue = document.querySelector("input").value;
+    let square = inputValue * inputValue;
+    alert("The square of " + inputValue + " is " + square);
+}
---
+++ q2.js
@@ -0,0 +1,8 @@
+
+function calculateCube() {
+
+    let inputValue = document.querySelector("input").value;
+    let cube = inputValue*inputValue*inputValue;
+    alert(cube);
+    console.log(inputValue);
+}
